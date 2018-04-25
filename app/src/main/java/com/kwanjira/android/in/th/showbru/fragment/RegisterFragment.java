f894b0469e808ca8c8f5f1bfc3ae334b8d5da685
kwanjira <waikhwanjira@gmail.com>
2018-04-25 17:05:07 +0700

    Replace Fragment Success

--- app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/RegisterFragment.java
@@ -4,13 +4,39 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kwanjira.android.in.th.showbru.MainActivity;
 import com.kwanjira.android.in.th.showbru.R;
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toobar
+        createToobar();
+
+    }   //Main Method
+
+    private void createToobar() {
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+//        Setup Navigation
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
 
     @Nullable
     @Override
