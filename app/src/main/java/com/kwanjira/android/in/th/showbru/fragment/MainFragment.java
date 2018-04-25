3f4f5a17e5cf568ffdbfc578e9a66838a969dc7a
kwanjira <waikhwanjira@gmail.com>
2018-04-25 16:28:25 +0700

    Replace Fragment Success

--- app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/MainFragment.java
+++ app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/MainFragment.java
@@ -7,10 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.kwanjira.android.in.th.showbru.R;
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Replace Freagment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contenMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+    }   //Main Method
 
     @Nullable
     @Override
