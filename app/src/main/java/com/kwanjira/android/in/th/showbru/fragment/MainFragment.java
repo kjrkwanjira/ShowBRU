5e14bc4e287086df976ebaebd1b63edfa1f3ed21
kwanjira <waikhwanjira@gmail.com>
2018-04-26 17:02:01 +0700

    Adapter OK

--- app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/MainFragment.java
+++ app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/MainFragment.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kwanjira.android.in.th.showbru.R;
+import com.kwanjira.android.in.th.showbru.utility.GetAllData;
+import com.kwanjira.android.in.th.showbru.utility.MyAlert;
+import com.kwanjira.android.in.th.showbru.utility.MyConstant;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainFragment extends Fragment{
 
@@ -18,6 +28,94 @@ public class MainFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+        registerController();
+
+//        Login Controller
+        loginController();
+
+
+    }   //Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText uerEditText = getView().findViewById(R.id.adtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = uerEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+
+                    MyAlert myAlert = new MyAlert(getContext());
+                    myAlert.normalDialog("Have Space",
+                            "Please Fill All Blank");
+                } else {
+//                    No Space
+                    MyConstant myConstant = new MyConstant();
+                    boolean b = true;
+                    String truePass = null, nameUser = null;
+                    MyAlert myAlert = new MyAlert(getActivity());
+
+                    try {
+                        GetAllData getAllData = new GetAllData(getActivity());
+                        getAllData.execute(myConstant.getUrlGetAllUser());
+
+                        String jsonString = getAllData.get();
+                        Log.d("26AprilV1", "JSON ==> " + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+                        
+                        for (int i=0; i<jsonArray.length(); i++) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                            if (userString.equals(jsonObject.get("User"))) {
+
+                                b = false;
+                                truePass = jsonObject.getString("Password");
+                                nameUser = jsonObject.getString("Name");
+                                
+                            }
+                            
+                        }
+
+
+                        if (b) {
+                            
+                            myAlert.normalDialog("User False",
+                                    "No User in my Database");
+
+                        } else if (passwordString.equals(truePass)) {
+                            Toast.makeText(getActivity(), "Welcome" +nameUser,
+                                    Toast.LENGTH_SHORT).show();
+
+                            getActivity().getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.contenMainFragment, new ServiceFragment())
+                                    .commit();
+
+                        } else {
+                            myAlert.normalDialog("Password False",
+                                    "Please Try Agains Password False");
+                        } 
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+    }
+
+    private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +128,7 @@ public class MainFragment extends Fragment{
                         .commit();
             }
         });
-
-    }   //Main Method
+    }
 
     @Nullable
     @Override
@@ -39,4 +136,5 @@ public class MainFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
+
 }  //Main Class
---
+++ app/src/main/java/com/kwanjira/android/in/th/showbru/utility/MyConstant.java
@@ -0,0 +1,15 @@
+package com.kwanjira.android.in.th.showbru.utility;
+
+public class MyConstant {
+
+    private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
+    private String urlGetAllUser = "http://androidthai.in.th/bru/getAllUser.php";
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
