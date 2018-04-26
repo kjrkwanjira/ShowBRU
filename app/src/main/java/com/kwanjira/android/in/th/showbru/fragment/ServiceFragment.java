ef965b3bbf2fd83ff89227601b06aab738c0ed2a
kwanjira <waikhwanjira@gmail.com>
2018-04-26 17:44:47 +0700

    Adapter OK

--- app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/ServiceFragment.java
+++ app/src/main/java/com/kwanjira/android/in/th/showbru/fragment/ServiceFragment.java
@@ -7,10 +7,68 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.kwanjira.android.in.th.showbru.R;
+import com.kwanjira.android.in.th.showbru.utility.FoodAdapter;
+import com.kwanjira.android.in.th.showbru.utility.GetAllData;
+import com.kwanjira.android.in.th.showbru.utility.MyConstant;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create ListView
+        createListView();
+
+    }   //Main Method
+
+    private void createListView() {
+
+        ListView listView = getView().findViewById(R.id.ListViewFood);
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlGetAllFood());
+
+            String jsonString = getAllData.get();
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] foodStrings = new String[jsonArray.length()];
+            String[] priceStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] imageStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                foodStrings[i] = jsonObject.getString("NameFood");
+                priceStrings[i] = jsonObject.getString("Price");
+                detailStrings[i] = jsonObject.getString("Detail");
+                imageStrings[i] = jsonObject.getString("ImagePath");
+
+            }
+
+            FoodAdapter foodAdapter = new FoodAdapter(getActivity(),
+                    imageStrings, foodStrings, priceStrings, detailStrings);
+            listView.setAdapter(foodAdapter);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
